33da1c19151d8c7410ff72b6a299125896da4d97
theguymat <42230087+theguymat@users.noreply.github.com>
2019-11-12 16:49:41 +1100

    button, text fields and updating text

--- app/src/main/java/com/example/a6454/week2/MainActivity.java
+++ app/src/main/java/com/example/a6454/week2/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.a6454.week2;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+       TextView label = findViewById(R.id.textView);
+       EditText entry = findViewById(R.id.editText);
+       TextInputEditText entry2 = findViewById(R.id.entry2);
+
+       label.setText(entry2.getText());
+
+
     }
 }
