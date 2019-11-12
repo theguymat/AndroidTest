2108c6001a02d89159f43b28af24153a03c61d3a
theguymat <42230087+theguymat@users.noreply.github.com>
2019-11-12 15:14:01 +1100

    base

---
+++ app/src/main/java/com/example/a6454/week2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a6454.week2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
